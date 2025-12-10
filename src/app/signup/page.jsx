b53705b53d109928ccc9da6f2525784257e246a0
LinkPlayer932 <linkplayer932@gmail.com>
2025-12-10 14:51:43 +0500

    Done

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -134,7 +134,7 @@ export default function SignUpPage() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch("https://nest-solution-backend.vercel.app/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
